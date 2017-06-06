a69f1fe0a0bcca110e3d3acf2bb06889c411d2c7
Solemn Monk <solemn.monk@gmail.com>
2017-06-05 20:11:03 -0500

    Adding 2 additional plugin shells

---
+++ kUtils.plugin.hookup-hotshot/src/kUtils/plugin/hookup_hotshot/Example.java
@@ -0,0 +1,10 @@
+package kUtils.plugin.hookup_hotshot;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
---
+++ kUtils.plugin.suicide-girls/src/kUtils/plugin/suicide_girls/Example.java
@@ -0,0 +1,10 @@
+package kUtils.plugin.suicide_girls;
+
+import org.osgi.service.component.annotations.*;
+
+@Component
+public class Example {
+
+	// TODO: class provided by template
+
+}
